fed1f4421aef5b10fabf01cf3a5dbfd7f09da00c
Malik R <maliktroc@outlook.com>
2021-10-25 20:08:20 -0400

    Updating notifications element

--- tss-client/src/Notification.js
+++ tss-client/src/Notification.js
@@ -1,21 +1,48 @@
 import React from "react";
+
 import "./Notification.css";
+import Card from "react-bootstrap/Card";
+
+import notificationImg from "./client-header-b.jpg";
+
 
 
 function Notification({payload}) {
 
     return (
-        <div className="notification">
-            <div className="img-wrapper">
+        <Card
+            variant="none"
+            className="notification" 
+        >
+            <Card.Img 
+                className="notification-img"
+                src={notificationImg} 
+                alt="Notification image" 
+            />
+            <Card.ImgOverlay
+                className="description-container"
+            >
+                <Card.Title className="title">{payload}</Card.Title>
+                <Card.Text className="description">
+                    You never really know, but when they know, you'll know. You know?
+                </Card.Text>
+            </Card.ImgOverlay>
+        </Card>
+
+
+
+
+        // <div className="notification">
+        //     <div className="img-wrapper">
                 
-            </div>
-            <div className="description">
-                <p className="title">{payload}</p>
-                <p className="brief">This is a notification!</p>
-            </div>
+        //     </div>
+        //     <div className="description">
+        //         <p className="title">{payload}</p>
+        //         <p className="brief">This is a notification!</p>
+        //     </div>
             
 
-        </div>
+        // </div>
     );
 }
 
