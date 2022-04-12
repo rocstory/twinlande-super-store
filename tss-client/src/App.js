8a836ad4fa26bed37804cea32238bc7cca6e98c9
Malik R <mrocstory@gmail.com>
2022-04-12 17:44:50 -0400

    removing header image

--- tss-client/src/App.js
+++ tss-client/src/App.js
@@ -1,24 +1,16 @@
-
 import NavBar from "./NavBar";
 import HomeMenu from "./HomeMenu";
 
-import "./App.css"
+import "./App.css";
 
 import headerImg from "./client-header-b.jpg";
 
 function App() {
   return (
     <div className="App">
-
-      <div className="header-wrapper">
-        <img src={headerImg} alt="twinlande header"></img>
-      </div>
-      <NavBar/>
+      <NavBar />
       <HomeMenu />
-
-
     </div>
-
   );
 }
 
