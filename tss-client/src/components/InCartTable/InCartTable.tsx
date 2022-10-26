67f6651d84975170bec730cf4d51dffbc3ea7e34
Malik R <mrocstory@gmail.com>
2022-10-26 13:26:30 -0400

    d46 - InCartTable styling

--- tss-client/src/components/InCartTable/InCartTable.tsx
+++ tss-client/src/components/InCartTable/InCartTable.tsx
@@ -2,7 +2,8 @@ import { useContext } from "react";
 import { TSSContext } from "../../contexts/TSSContext";
 import { Table } from "react-bootstrap"
 import InCartItem from "./InCartItem/InCartItem";
-// import "./InCartTable.scss"
+import "./InCartTable.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 
@@ -12,6 +13,7 @@ function InCartTable() {
     } = useContext(TSSContext)
 
     let items = ['a','b','c'] // cartList
+    const numOfItems = 12
 
 
     return (
@@ -19,14 +21,23 @@ function InCartTable() {
             <Table>
                 <thead>
                     <tr>
-                        Shipping
+                        <div className="incart-header">
+                            <div className="icon-wrapper ">
+                                <span className="brand-icon">
+                                    <FontAwesomeIcon icon={["fas", "seedling"]} />
+                                </span>
+                            </div>
+                            <div className="content-wrapper">
+                                <span>Shipping</span>
+                                <span>{numOfItems} items</span>
+                            </div>
+                        </div>
                     </tr>
                 </thead>
                 <tbody>
                     {
                         items.map(item => <InCartItem key={item} item={item} />)
                     }
-                    
                 </tbody>
 
             </Table>
